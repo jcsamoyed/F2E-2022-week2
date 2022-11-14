fafed2bb4226c8659ec5f41a0e3e40839bc9440c
jocelyn <jocelyn@ecloudmobile.com>
2022-11-14 17:51:21 +0800

    feat: 新增 header 區塊

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="common-layout">
+    <el-container>
+      <el-header height="auto">
+        <router-link to="/" class="login-btn">
+          <el-icon size="24" color="#fff"><UserFilled /></el-icon>
+          <span>會員登入</span>
+        </router-link>
+        <router-link to="/">
+          <img src="@/assets/images/common/logo.svg" class="logo" alt="Master Sign logo" />
+        </router-link>
+        <el-icon size="24" color="#fff" class="hamburger">
+          <Fold v-if="true" />
+          <Expand v-else />
+        </el-icon>
+      </el-header>
+      <el-container>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
+<script setup>
+import { UserFilled, Expand, Fold } from '@element-plus/icons-vue';
+</script>
+
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans TC', sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
