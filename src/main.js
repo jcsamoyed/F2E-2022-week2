74af455ba9cdb1b7c13cc48eb86928d2741fa240
jocelyn <jocelyn@ecloudmobile.com>
2022-11-18 22:36:20 +0800

    build: 安裝 mitt

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import '@/assets/scss/main.scss';
 import ElementPlus from 'element-plus';
+import mitt from 'mitt';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(ElementPlus).use(store).use(router)
-  .mount('#app');
+const eventBus = mitt();
+const app = createApp(App);
+
+app.config.globalProperties.eventBus = eventBus;
+app.use(ElementPlus).use(store).use(router).mount('#app');
